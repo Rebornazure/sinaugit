484c3964d307f86bb219d5a676e740e4566061ae
Rebornazure <galuhadjiabiyyuakbar@gmail.com>
2026-05-11 01:20:42 +0700

    fix: complete game flow and UI panel management

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -3,18 +3,41 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("Panels")]
+    public GameObject pausePanel;
+    public GameObject gameOverPanel;
+
+    void Update()
+    {
+        if (pausePanel != null)
+            pausePanel.SetActive(GameManager.Instance.currentState == GameState.Paused);
+            
+        if (gameOverPanel != null)
+            gameOverPanel.SetActive(GameManager.Instance.currentState == GameState.GameOver);
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("Game");
     }
 
-    public void QuitGame()
+    public void Resume()
     {
-        Application.Quit();
+        GameManager.Instance.ResumeGame();
     }
 
     public void Restart()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
